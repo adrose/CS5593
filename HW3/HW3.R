1fa5be7bfe07b6446056be1d25b63d9d9aed34e9
Adon <adon.rosen@gmail.com>
2020-11-04 20:17:58 -0600

    Adding HW3 - very difficult

---
+++ HW3/HW3.R
@@ -0,0 +1,269 @@
+# ---- load-packages --------------------------------------------------------
+rm(list = ls())
+library(tidyverse)
+library(psych)
+library(foreach)
+library(doParallel)
+library(ggplot2)
+library(reshape2)
+library(caret)
+library(progress)
+library(arules)
+library(arulesViz)
+library(knitr)
+library(kableExtra)
+library(gtools)
+
+# ---- problem-1-support --------------------------------------------------------
+in.data <- read.csv('./hw3Q1.csv')
+in.data.orig <- in.data
+in.data[in.data==0] <- FALSE
+in.data[in.data==1] <- TRUE
+in.data <- apply(in.data, 2, as.logical)
+
+## Now do it manually
+# First create the item sets
+one.pair <- c('a', 'b', 'c', 'd', 'e')
+two.pair <- combinations(r = 2, v = c("a", "b", "c", "d", "e"), repeats.allowed = FALSE, n = 5)
+thr.pair <- combinations(r = 3, v = c("a", "b", "c", "d", "e"), repeats.allowed = FALSE, n = 5)
+fou.pair <- combinations(r = 4, v = c("a", "b", "c", "d", "e"), repeats.allowed = FALSE, n = 5)
+fiv.pair <- combinations(r = 5, v = c("a", "b", "c", "d", "e"), repeats.allowed = FALSE, n = 5)
+
+## Now go through these and calculate the support
+out.support <- NULL
+for(i in one.pair){
+  ## Calculate the count of each of these columns in the data
+  out.count <- sum(in.data[,i])
+  out.sup <- out.count/dim(in.data)[1]
+  out.row <- c(i, out.count, out.sup)
+  out.support <- rbind(out.support, out.row)
+}
+## Now do two pairs
+for(i in 1:dim(two.pair)[1]){
+  ## Calc the support
+  vals <- two.pair[i,]
+  out.count <- length(which(in.data[,vals[1]] & in.data[,vals[2]]))
+  out.sup <- out.count/dim(in.data)[1]
+  out.row <- c(paste(vals, collapse ="_"), out.count, out.sup)
+  out.support <- rbind(out.support, out.row)
+}
+## Now do 3 pair
+for(i in 1:dim(thr.pair)[1]){
+  ## Calc the support
+  vals <- thr.pair[i,]
+  out.count <- length(which(in.data[,vals[1]] & in.data[,vals[2]] & in.data[,vals[3]]))
+  out.sup <- out.count/dim(in.data)[1]
+  out.row <- c(paste(vals, collapse ="_"), out.count, out.sup)
+  out.support <- rbind(out.support, out.row)
+}
+## Now do the 4 pair
+for(i in 1:dim(fou.pair)[1]){
+  ## Calc the support
+  vals <- fou.pair[i,]
+  out.count <- length(which(in.data[,vals[1]] & in.data[,vals[2]] & in.data[,vals[3]] & in.data[,vals[4]]))
+  out.sup <- out.count/dim(in.data)[1]
+  out.row <- c(paste(vals, collapse ="_"), out.count, out.sup)
+  out.support <- rbind(out.support, out.row)
+}
+## Now the final 5 pair
+for(i in 1:dim(fiv.pair)[1]){
+  ## Calc the support
+  vals <- fiv.pair[i,]
+  out.count <- length(which(in.data[,vals[1]] & in.data[,vals[2]] & in.data[,vals[3]] & in.data[,vals[4]] & in.data[,vals[5]]))
+  out.sup <- out.count/dim(in.data)[1]
+  out.row <- c(paste(vals, collapse ="_"), out.count, out.sup)
+  out.support <- rbind(out.support, out.row)
+}
+## Now print the support of these
+out.support <- as.data.frame(out.support)
+out.support$V3 <- as.numeric(as.character(out.support$V3))
+kable(out.support, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3) %>%
+  kable_styling(full_width = FALSE)
+
+# ---- problem-1-supportPass --------------------------------------------------------
+out.support2 <- as.data.frame(out.support)
+out.support2 <- out.support2[which(out.support2$V3>=.35),]
+kable(out.support2, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3) %>%
+  kable_styling(full_width = FALSE)
+
+# ---- problem-1-maximal --------------------------------------------------------
+fsets <- eclat(in.data, parameter = list(supp = 0.35))
+fsets.maximal <- fsets[is.maximal(fsets)]
+fsets.closed <- fsets[is.closed(fsets)]
+
+out.support3 <- out.support2
+out.support3$isMaximal <- FALSE
+out.support3$isMaximal[c(10,8,7)] <- TRUE
+
+
+out.support3$isClosedFrequnet <- FALSE
+out.support3$isClosedFrequnet[c(5,6)] <- TRUE
+
+out.support3$isFrequnetButNotClosedOrMaimxal <- FALSE
+out.support3$isFrequnetButNotClosedOrMaimxal[9] <- TRUE
+
+kable(out.support3, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3) %>%
+  kable_styling(full_width = FALSE)
+
+# ---- problem-1-infrequent --------------------------------------------------------
+out.support4 <- as.data.frame(out.support)
+out.support4 <- out.support4[which(out.support4$V3<.35),]
+kable(out.support4, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3) %>%
+  kable_styling(full_width = FALSE)
+
+# ---- problem-2-contingency1 --------------------------------------------------------
+out.tab <- table(in.data[,'b'], in.data[,'e'])
+rownames(out.tab) <- c("B-", "B")
+colnames(out.tab) <- c("E-", "E")
+kable(out.tab, 
+  format    = "latex", 
+  longtable = T, 
+  digits = 3)
+
+# ---- problem-2-contingency2 --------------------------------------------------------
+out.tab <- table(in.data[,'a'], in.data[,'d'])
+rownames(out.tab) <- c("A-", "A")
+colnames(out.tab) <- c("D-", "D")
+kable(out.tab, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3)
+
+# ---- problem-2-contingency3 --------------------------------------------------------
+out.tab <- table(in.data[,'e'], in.data[,'d'])
+rownames(out.tab) <- c("E-", "E")
+colnames(out.tab) <- c("D-", "D")
+kable(out.tab, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3)
+
+# ---- problem-2-results1 --------------------------------------------------------
+## First do support
+row.one.s <- c("B>E", "Support", round(5/12, 2),3)
+row.two.s <- c("A>D", "Support", round(6/12,2), 2)
+row.thr.s <- c("E>D", "Support", round(8/12,2), 1)
+out <- rbind(row.one.s, row.two.s, row.thr.s)
+kable(out, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3,
+      col.names = c("Rule", "Measure", "Value", "Rank"))
+# ---- problem-2-results2 --------------------------------------------------------
+## Now do confidence
+row.one.c <- c("B>E", "Confidence", round(5/7, 2),3)
+row.two.c <- c("A>D", "Confidence", round(6/7, 2),1)
+row.thr.c <- c("E>D", "Confidence", round(8/10,2), 2)
+out <- rbind(row.one.c, row.two.c, row.thr.c)
+kable(out, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3,
+      col.names = c("Rule", "Measure", "Value", "Rank"))
+
+# ---- problem-2-results3 --------------------------------------------------------
+# Now do the interest
+row.one.i <- c("B>E", "Interest", round((5 / 12 ) / ((10 / 12) * (7/12)),2), 3)
+row.two.i <- c("A>D", "Interest", round((6 / 12 ) / ((9 / 12) * (7/12)), 2),1)
+row.thr.i <- c("E>D", "Interest", round((8 / 12 ) / ((9 / 12) * (10/12)),2), 2)
+out <- rbind(row.one.i, row.two.i, row.thr.i)
+kable(out, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3,
+      col.names = c("Rule", "Measure", "Value", "Rank"))
+
+
+# ---- problem-2-results4 --------------------------------------------------------
+# Now do the interest
+row.one.i <- c("B>E", "IS", round((5/12)/sqrt((7/12) * (10/12)),2), 3)
+row.two.i <- c("A>D", "IS", round((6/12)/sqrt((7/12) * (9/12)), 2),2)
+row.thr.i <- c("E>D", "IS", round((8/12)/sqrt((10/12) * (9/12)),2), 1)
+out <- rbind(row.one.i, row.two.i, row.thr.i)
+kable(out, 
+      format    = "latex", 
+      longtable = T, 
+      digits = 3,
+      col.names = c("Rule", "Measure", "Value", "Rank"))
+
+# ---- problem-3 --------------------------------------------------------
+# Load the data
+in.data <- read.table("./Data/HW3_data.txt", fill = TRUE)
+in.data[in.data==""] <- NA
+in.data <- apply(in.data, c(1,2), as.character)
+# Cretae a parallel environment
+registerDoParallel(8)
+# ---- problem-3-bII --------------------------------------------------------
+ModifiedApriori <- function(data, n=3, sup=0.003){
+  ## This function will identify all possible candidate itemsets of sizes =< n
+  ## The first thing necessary is to create all possible pairs
+  # First identify all elements
+  all.element <- unique(unlist(data))
+  all.element <- names(table(all.element))
+  all.element <- all.element
+  threshold <- sup*nrow(data)
+  ## Now go through every transaction and add it ato a set of all transactions if it hasn't been counted
+  all.trans <- matrix(NA, nrow=length(all.element), ncol=2)
+  index <- 1
+  for(i in all.element){
+    # Identify itemsets and their frequency
+    count <- sum(data == i, na.rm=T)
+    all.trans[index,] <- c(i, count)
+    index <- index + 1
+  }
+  ## Now remove those who do not meet threshold
+  all.trans <- all.trans[-which(as.numeric(all.trans[,2]) < threshold),]
+  ## Now in a loop go through and compute all supersets from 2..n
+  all.element.n <- list()
+  all.element.n[[1]] <- all.trans[,1]
+  all.trans.np <- list()
+  all.trans.np[[1]] <- all.trans
+  index <- 1
+  for(q in 2:n){
+    all.element.n[[q]] <- combn(all.trans[,1], q)
+    ## Now remove those that are not in the trimmed 2 pair
+    if(is.null(dim(all.trans.np[[q-1]]))){
+      to.rm <- which(!t(all.element.n[[q]])[,1:q-1] %in% all.trans.np[[q-1]][seq(1,q-1,1)])
+    }else{
+      to.rm <- which(!t(all.element.n[[q]])[,1:q-1] %in% all.trans.np[[q-1]][,seq(1,q-1,1)])
+    }
+    if(length(to.rm)>0){
+      all.element.n[[q]] <- all.element.n[[q]][,-to.rm]
+    }
+    all.trans.n <- foreach(i=1:ncol(all.element.n[[q]]), .combine=rbind) %dopar%{
+      call <- NULL
+      for(d in 1:q){
+        if(d==1){
+          call <- paste(call, "all.element.n[[q]][", d,",", i, "] %in% x")
+        }else{
+          call <- paste(call, "& all.element.n[[q]][", d,",", i, "] %in% x")
+        }
+      }
+      count <- sum(apply(data, 1, function(x) eval(parse(text=call))))
+      c(paste(all.element.n[[q]][,i]), count)
+    }
+    all.trans.n <- all.trans.n[-which(as.numeric(all.trans.n[,q+1]) < threshold),]
+    all.trans.np[[q]] <- all.trans.n
+    index <- index + 1
+  }
+  ## Now return all of these
+  out.vals <- list(out.items <- all.element.n, out.supp <- all.trans.np, n<-n, sup<-sup)
+}
+
+RuleGeneration <- function(out.vals){
+  ## This function will organize the support and counts for all of the items
+  ## from the output of the ModifiedApriori function
+  
+}
